0526e8a1378b312e78d62d0dc7618ce39503282f
alphasploit <60095230+A3lpha@users.noreply.github.com>
2023-09-24 03:15:50 -0700

    Add files via upload

---
+++ Day 2/integer_floats.py
@@ -0,0 +1,6 @@
+#my_number = 1 + 5
+#print(my_number + my_number)
+
+#print(type(my_number))
+age = input("Tell me your age: ")
+print("your age is "+ age)
---
+++ Day 2/variable.py
@@ -0,0 +1,10 @@
+#ame = "adnan"
+#print(name)
+#name = "jacfar"
+#print(name)
+#name = input("Tell me your name: ")
+#print("Your name is " + name)
+name = "hello "
+age = "52"
+total = name + age
+print(total)
